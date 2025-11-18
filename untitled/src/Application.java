060579cf9a1bf3324c148c49a2381e264aa00d95
Mohamed <mahadaalin@gmail.com>
2025-11-18 15:54:36 +0300

    main commit 1
    Initialize Application class with main frame and panel placeholders

---
+++ untitled/src/Application.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.sql.*;
+
+public class Application {
+    private JFrame frame;
+    private JPanel mainPanel;
+
+    public Application() {
+
+    }
+}
